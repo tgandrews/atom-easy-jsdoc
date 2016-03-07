af656f2af82fcd423d531cc65fe9d9d034f1360a
Tom Andrews <git@tomandrews.co.uk>
2016-03-07 19:35:59 +0000

    Support params

--- tests/commentator.test.js
+++ tests/commentator.test.js
@@ -9,10 +9,26 @@ describe('Commentator', () => {
       comment.should.be.a('function');
     });
 
-    it('should be able to create a comment', () => {
+    it('should be able to create a comment without params', () => {
       const code = `function helloWorld () {}`;
       const doc = `/**
 * helloWorld - Description
+*
+* @returns {type} Description
+**/`;
+
+      const result = comment(code);
+      result.should.equal(doc);
+    });
+
+    it('should be able to create a comment with params', () => {
+      const code = `function helloWorld (a, b, c) {}`;
+      const doc = `/**
+* helloWorld - Description
+*
+* @param a {type} Description
+* @param b {type} Description
+* @param c {type} Description
 *
 * @returns {type} Description
 **/`;
