7e8d481f547b7d79b0d4db52772a2cb446ab09b1
Tom Andrews <git@tomandrews.co.uk>
2016-08-16 15:17:22 +0100

    Add support for parameters with parents or with default value

--- tests/unit/jsdoc/parser.test.js
+++ tests/unit/jsdoc/parser.test.js
@@ -52,43 +52,79 @@ describe('JSDoc Paser', () => {
       parse(doc).returns.should.eql(returns);
     });
 
-    it('should extract a parameter', () => {
-      const doc = `/**
- * helloWorld - Here is a description
- *
- * @param {string} a A does a thing
- *
- * @returns {type} Description
- */`;
-      const params = [{
-        type: 'string',
-        name: 'a',
-        description: 'A does a thing',
-      }];
 
-      parse(doc).params.should.eql(params);
-    });
+    describe('parameters', () => {
+      it('should extract a parameter', () => {
+        const doc = `/**
+   * helloWorld - Here is a description
+   *
+   * @param {string} a A does a thing
+   *
+   * @returns {type} Description
+   */`;
+        const params = [{
+          type: 'string',
+          name: 'a',
+          description: 'A does a thing',
+        }];
 
-    it('should extract multiple parameters', () => {
-      const doc = `/**
- * helloWorld - Here is a description
- *
- * @param {string} a A does a thing
- * @param {object} b B does something else
- *
- * @returns {type} Description
- */`;
-      const params = [{
-        type: 'string',
-        name: 'a',
-        description: 'A does a thing',
-      }, {
-        type: 'object',
-        name: 'b',
-        description: 'B does something else',
-      }];
+        parse(doc).params.should.eql(params);
+      });
 
-      parse(doc).params.should.eql(params);
+      it('should extract multiple parameters', () => {
+        const doc = `/**
+   * helloWorld - Here is a description
+   *
+   * @param {string} a A does a thing
+   * @param {object} b B does something else
+   *
+   * @returns {type} Description
+   */`;
+        const params = [{
+          type: 'string',
+          name: 'a',
+          description: 'A does a thing',
+        }, {
+          type: 'object',
+          name: 'b',
+          description: 'B does something else',
+        }];
+
+        parse(doc).params.should.eql(params);
+      });
+
+      it('should extract parameters with a parent', () => {
+        const doc = `/**
+   * helloWorld - Here is a description
+   *
+   * @param {string} b.a A does a thing
+   *
+   * @returns {type} Description
+   */`;
+        const params = [{
+          type: 'string',
+          name: 'a',
+          parent: 'b',
+          description: 'A does a thing',
+        }];
+
+        parse(doc).params.should.eql(params);
+      });
+
+      it('should extract default values', () => {
+        const doc = `/**
+   * helloWorld - Here is a description
+   *
+   * @param {number} [a=1] A does a thing
+   *
+   * @returns {type} Description
+   */`;
+
+        const params = [
+          { name: 'a', defaultValue: '1', type: 'number', description: 'A does a thing' },
+        ];
+        parse(doc).params.should.eql(params);
+      });
     });
   });
 });
